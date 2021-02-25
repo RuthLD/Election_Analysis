10734c5654bc21979a399edb8869b4587968e694
RuthLD <78384372+RuthLD@users.noreply.github.com>
2021-02-25 17:10:12 -0500

    Added png files for Summary
    Added the images of code for the readme summary.

---
+++ Election_Analysis/Python_practice.py
@@ -0,0 +1,46 @@
+#print("Hello World!")
+
+#print(1.5+2*3)
+#counties=["Arapahoe","Denvor","Jefferson"]
+#print (counties)
+#print(counties[0])
+#print(len(counties))
+#print(counties[0:2])
+#counties.append("El Paso")
+#print(counties)
+#counties_tuple = ("Arapahoe","Denvor","Jefferson")
+#print(len(counties_tuple))
+#counties_dict={}
+#counties_dict["Arapahoe"] = 422829
+#counties_dict["Denver"]= 463353
+#counties_dict["Jefferson"]=432438
+#voting_data=[]
+#voting_data.append({"county":"Arapahoe","registered_voters":422829})
+#voting_data.append({"county":"Devner","registered_voters":463343})
+#voting_data.append({"county":"Jefferson","registered_voters":431438})
+
+#counties_dict = {"Arapahoe": 369237, "Denver":413229, "Jefferson": 390222}
+#for county, voters in counties_dict.items():
+    #print(county + " county has " + str(voters) + " registered voters.")
+
+#for county, voters in counties_dict.items():
+   # print(f"{county} county has {voters} registered voters.")
+
+# candidate_votes = int(input("How many votes did the candidate get in the election? "))
+# total_votes = int(input("What is the total number of votes in the election? "))
+# message_to_candidate = (
+    #f"You received {candidate_votes:,} number of votes. "
+   # f"The total number of votes in the election was {total_votes:,}. "
+    #f"You received {candidate_votes / total_votes * 100:.2f}% of the total votes.")
+
+#print(message_to_candidate)
+
+# Import the datetime class from the datetime module.
+#import datetime as dt
+# Use the now() attribute on the datetime class to get the present time.
+#now = dt.datetime.now()
+# Print the present time.
+#print("The time right now is ", now)
+
+import csv
+dir(csv)
